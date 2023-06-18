62b7f26d07d18b74867d9bc2ecd8f421c8f62319
Sohom Chowdhury <sohom138@gmail.com>
2023-06-18 14:02:46 +0530

    removing the java files

--- diff.java
+++
@@ -1,6 +0,0 @@
-public class diff{
-	public static void main(String args[])
-	{
-		System.out.println("DIFFERENCE");
-	}
-}
--- multiply.java
+++
@@ -1,8 +0,0 @@
-public class multiply{
-	public static void main(String args[])
-	{
-		int a=5;
-		int b=5;
-		System.out.println("The product of the two numbers is :"+(a*b));
-	}
-}
